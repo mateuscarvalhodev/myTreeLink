6d9ce860cc36d9f41e72c81a077c7d383a123073
Mateus Carvalho <mateuscarvalhodev@outlook.com>
2022-10-07 15:18:26 -0300

    1.0

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <div>
+      <img className="avatar" src="https://github.com/mateuscarvalhodev.png" alt="myPhoto" />
+        <h1>Mateus Carvalho</h1>
+        <p className="userName">@mateuscarvalho1</p>
+        
+      </div>
+      <ul>
+            <li>
+                <a href="https://instagram.com/mateuscarvalho1" target="_blank">📷 Instagram</a>
+            </li>
+            <li>
+                <a href="https://github.com/mateuscarvalhodev" target="_blank">🐱 GitHub</a>
+            </li>
+            <li>
+                <a href="https://wa.me/5585998635544" target="_blank">📞 WhatsApp</a>
+            </li>
+        </ul>
+        <footer>
+            Made with 💜 by Mateus Carvalho.
+        </footer>
+      
+      
+    </div>
+  )
+}
+
+export default App
